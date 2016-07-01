6a624adc3d5027130bd6ec63897bee8852a3ff09
Michael Jackson <mjijackson@gmail.com>
2016-06-30 22:10:57 -0700

    Add episode 004

--- modules/PodcastInfo.js
+++ modules/PodcastInfo.js
@@ -12,7 +12,7 @@ export const episodes = [
     title: 'Getting Into React',
     description: 'In this episode, we introduce ourselves, talk about the goals of React30, and discuss how each of us came to React',
     descriptionHTML: 'In this episode, we introduce ourselves, talk about the goals of <a href="https://twitter.com/react30">React30</a>, and discuss how each of us came to <a href="https://facebook.github.io/react">React</a>',
-    audioURL: 'http://files.react30.com/001_React30_-_Getting_Into_React.mp3',
+    audioURL: 'http://files.react30.com/001_React30_Getting_Into_React.mp3',
     YouTubeURL: 'https://www.youtube.com/watch?v=9IBFs5qkVAs',
     duration: '00:30:00',
     size: 21601323,
@@ -23,7 +23,7 @@ export const episodes = [
     title: 'Streaming React',
     description: 'Our guest in this episode is Sasha Aickin, recently-retired CTO of Redfin and author of react-dom-stream and several pull requests that enable streaming rendering in React core',
     descriptionHTML: 'Our guest in this episode is <a href="https://twitter.com/xander76">Sasha Aickin</a>, recently-retired CTO of Redfin and author of <a href="https://www.npmjs.com/package/react-dom-stream">react-dom-stream</a> and several pull requests that enable streaming rendering in React core',
-    audioURL: 'http://files.react30.com/002_React30_-_Streaming_React.mp3',
+    audioURL: 'http://files.react30.com/002_React30_Streaming_React.mp3',
     YouTubeURL: 'https://www.youtube.com/watch?v=XW_c60NCkI4',
     duration: '00:30:00',
     size: 28801433,
@@ -38,6 +38,17 @@ export const episodes = [
     YouTubeURL: 'https://www.youtube.com/watch?v=fwT9kHOkig8',
     duration: '00:30:45',
     size: 22150453,
+    type: 'audio/mpeg'
+  },
+  { id: '004',
+    date: createDate('June 29, 2016'),
+    title: 'React Hardware',
+    description: 'In this episode we explore the possibilities of using React components to control real hardware components with the author of React Hardware, Dustan Kasten',
+    descriptionHTML: 'In this episode we explore the possibilities of using React components to control real hardware components with the author of <a href="https://github.com/iamdustan/react-hardware">React Hardware</a>, <a href="https://twitter.com/iamdustan">Dustan Kasten</a>',
+    audioURL: 'http://files.react30.com/004_React30_React_Hardware.mp3',
+    YouTubeURL: 'https://www.youtube.com/watch?v=-p3nbKf11zQ',
+    duration: '00:31:29',
+    size: 22673636,
     type: 'audio/mpeg'
   }
 ].sort(byDateDescending)
